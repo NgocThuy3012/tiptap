06987c97bc3f3f968ef080b0f07e642ce3222702
NgocThuy3012 <tranngocthuy3012@gmail.com>
2022-10-13 09:24:18 +0700

    table, youtube

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -4,10 +4,16 @@ import TextAlign from "@tiptap/extension-text-align"
 import { EditorContent, useEditor } from "@tiptap/react"
 import StarterKit from "@tiptap/starter-kit"
 import { useCallback } from "react"
-import {BiBold, BiItalic, BiListUl, BiListOl, BiRedo, BiUndo} from "react-icons/bi"
-import {BsImage} from "react-icons/bs"
-import {MdFormatAlignRight, MdFormatAlignLeft} from "react-icons/md"
+import {BiBold, BiItalic, BiListUl, BiListOl, BiRedo, BiUndo, BiTable} from "react-icons/bi"
+import {BsImage, BsYoutube} from "react-icons/bs"
+import {MdFormatAlignRight, MdFormatAlignLeft, MdKeyboardArrowDown} from "react-icons/md"
 import {RiLinkM} from "react-icons/ri"
+import {GrTableAdd} from "react-icons/gr"
+import Table from "@tiptap/extension-table"
+import TableRow from "@tiptap/extension-table-row"
+import TableHeader from "@tiptap/extension-table-header"
+import TableCell from "@tiptap/extension-table-cell"
+import Youtube from "@tiptap/extension-youtube"
 
 const TipTap = () => {
     const editor = useEditor({
@@ -20,11 +26,56 @@ const TipTap = () => {
             Link.configure({
                 openOnClick: false,
             }),
+            Table.configure({
+                resizable: true,
+            }),
+            TableRow,
+            TableHeader,
+            TableCell,
+            Youtube.configure({
+                controls: false,
+            }),
           ],
         content: `
-            <div className="content">
+            <div className="content ">
                 <div>afihuir</div>
                 <div>zcrhnnsgygf76eR</div>
+                <h3>
+                    Have you seen our tables? They are amazing!
+                </h3>
+                <ul>
+                    <li>tables with rows, cells and headers (optional)</li>
+                    <li>support for <code>colgroup</code> and <code>rowspan</code></li>
+                    <li>and even resizable columns (optional)</li>
+                </ul>
+                <p>
+                    Here is an example:
+                </p>
+                <table>
+                    <tbody>
+                    <tr>
+                        <th>Name</th>
+                        <th colspan="3">Description</th>
+                    </tr>
+                    <tr>
+                        <td>Cyndi Lauper</td>
+                        <td>singer</td>
+                        <td>songwriter</td>
+                        <td>actress</td>
+                    </tr>
+                    <tr>
+                        <td>Marie Curie</td>
+                        <td>scientist</td>
+                        <td>chemist</td>
+                        <td>physicist</td>
+                    </tr>
+                    <tr>
+                        <td>Indira Gandhi</td>
+                        <td>prime minister</td>
+                        <td colspan="2">politician</td>
+                    </tr>
+                    </tbody>
+                </table>
             </div>
         `
     })
@@ -60,13 +111,23 @@ const MenuBar = ({editor}) => {
         editor.chain().focus().extendMarkRange('link').setLink({ href: url }).run()
       }, [editor])
 
+      const addYoutubeVideo = () => {
+        const url = prompt('Enter YouTube URL')
+    
+        editor.commands.setYoutubeVideo({
+          src: url,
+          width: 640,
+          height: 480,
+        })
+      }
+
     if(!editor){
         return null
     }
     return (
         <div>
-            <div className="dropdown">
-                <span className="dropbtn">Heading...</span>
+            <button className="dropdown">
+                <span className="dropbtn">Heading... <MdKeyboardArrowDown/></span>
                 <div className="dropdown-content">
                     <div onClick={()=>editor.chain().focus().toggleHeading({level: 1}).run()}>Heading 1</div>
                     <div onClick={()=>editor.chain().focus().toggleHeading({level: 2}).run()}>Heading 2</div>
@@ -76,7 +137,7 @@ const MenuBar = ({editor}) => {
                     <div onClick={()=>editor.chain().focus().toggleHeading({level: 6}).run()}>Heading 6</div>
                 </div>
                 
-            </div>
+            </button>
             <button onClick={()=>editor.chain().focus().toggleHeading({ level: 1 }).run()}>h1</button>
             <button 
                 onClick={()=>editor.chain().focus().toggleBold().run()}
@@ -119,6 +180,60 @@ const MenuBar = ({editor}) => {
             >
                 <MdFormatAlignLeft/>
             </button>
+            <button className="dropdown dropbtn">
+                <BiTable className=""/>
+                <MdKeyboardArrowDown/>
+                <div className="dropdown-content">
+                    <div onClick={() => editor.chain().focus().insertTable({ rows: 3, cols: 3, withHeaderRow: true }).run()
+        }>
+                        <GrTableAdd/>
+                    </div>
+                    <div 
+                        onClick={() => editor.chain().focus().deleteTable().run()}
+                        disabled={!editor.can().deleteTable()}
+                    >
+                        DeleteTable
+                        
+                    </div>
+                    <div 
+                        onClick={() => editor.chain().focus().addColumnBefore().run()} 
+                        disabled={!editor.can().addColumnBefore()}
+                    >
+                        AddColumnBefore
+                    </div>
+                    <div 
+                        onClick={() => editor.chain().focus().addRowBefore().run()} 
+                        disabled={!editor.can().addRowBefore()}
+                    >
+                        AddRowBefore
+                    </div>
+                    <div 
+                        onClick={() => editor.chain().focus().deleteColumn().run()} 
+                        disabled={!editor.can().deleteColumn()}
+                    >
+                        DeleteColumn
+                    </div>
+                    <div 
+                        onClick={() => editor.chain().focus().deleteRow().run()} 
+                        disabled={!editor.can().deleteRow()}
+                    >
+                        DeleteRow
+                    </div>
+                </div>
+            </button>
+            <button
+                onClick={addImage}
+            >
+                <BsImage/>
+            </button>
+            <button className="dropdown dropbtn" >
+                <BsYoutube/>
+                <MdKeyboardArrowDown/>
+                <div onClick={addYoutubeVideo} className="dropdown-content">
+                    <div>AddYoutube</div>
+                </div>
+                
+            </button>
             <button
                 onClick={() => editor.chain().focus().undo().run()}
             >
@@ -129,11 +244,7 @@ const MenuBar = ({editor}) => {
             >
                 <BiRedo/>
             </button>
-            <button
-                onClick={addImage}
-            >
-                <BsImage/>
-            </button>
+           
            
         </div>
     )
